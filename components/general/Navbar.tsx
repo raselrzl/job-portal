3013eaeb0d05d14bf70566b139d25121d33f7ba9
Rasel Miah <rasel6041@gmail.com>
2025-04-28 10:10:02 +0100

    installed theme

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
 
 export default function Navbar() {
   return (
    <nav className=" flex items-center justify-between py-5">
-        <Link href="/">
-            <h1 className="text-2xl font-bold">Job<span className="text-primary">Lynk</span></h1>
+     <Link href="/" className="flex items-center gap-2">
+        <Image
+            src="/job.png"
+            height={20}
+            width={40}
+            alt="logo image"
+        />       
+            <h1 className="text-2xl font-bold"><span className="text-primary">Lynk</span></h1>
         </Link>
+       <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <Button>Login</Button>
+       </div>
    </nav>
   );
 }
