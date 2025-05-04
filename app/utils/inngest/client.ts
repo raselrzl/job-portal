26ec0fe5cde0791cfe9f3ef428bc73326196987b
Rasel Miah <rasel6041@gmail.com>
2025-05-04 01:44:04 +0100

    inngest testing 1m

--- app/utils/inngest/client.ts
+++ app/utils/inngest/client.ts
@@ -2,5 +2,5 @@ import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
   id: "Job-Lynk",
-  eventKey: process.env.INNGEST_SIGNING_KEY,
+  /* eventKey: process.env.INNGEST_SIGNING_KEY as string, */
 });
