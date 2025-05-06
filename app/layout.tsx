f9558ef8272806358917ee0cf23a6ce6948f0b0f
Rasel Miah <rasel6041@gmail.com>
2025-05-06 16:39:58 +0100

    responsive ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,8 +40,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="mx-w-7xl mx-auto md:px-6 lg:px-8">
-            <div className="fixed top-0 left-0 right-0 z-50 pl-5 md:px-20 bg-background shadow-md">
+          <div className="">
+            <div className="fixed top-0 left-0 right-0 z-50 px-5 md:px-20 bg-background shadow-md">
               <Navbar />
             </div>
           </div>
